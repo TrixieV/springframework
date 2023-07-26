7dc67cd61333b6d265863870d029be4702d288e0
titi <thaophuong5625@gmail.com>
2023-07-26 16:45:01 +0200

    Addding practice9 - Thymeleaf

---
+++ practice9/src/main/java/com/example9/practice9/Practice9Application.java
@@ -0,0 +1,13 @@
+package com.example9.practice9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice9Application.class, args);
+    }
+
+}
--- practice9/src/main/java/com/example9/practice9/controller/HomeController.java
+++ practice9/src/main/java/com/example9/practice9/controller/HomeController.java
@@ -1,14 +1,15 @@
-package com.example8.practice8.controller;
+package com.example9.practice9.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomepage() {
-//        model.addAttribute("username", "John Doe");
+    public String displayHomepage(Model model) {
+        model.addAttribute("username", "Row");
         return "home.html";
     }
 }
